cabae3c6f44b54a712306d26b9e18d7aa07c474b
Ahmed Abuwarda <zzz.sleep.com@gmail.com>
2022-10-06 19:15:53 +0300

    update some methods to user

--- controllers/v1/userController.js
+++ controllers/v1/userController.js
@@ -3,35 +3,35 @@ const userService = require('../../services/v1/userService.js');
 // route /api/v1/users
 // get all users from db
 exports.index = async (req, res, next) => {
-  return res.json(await userService.find());
+  return res.json(await userService.find(req));
 };
 
 // route /api/v1/users/:id
 // get specific users from db using id
 exports.show = async (req, res, next) => {
-  const id = req.params.id;
-  return res.json(await userService.findById(id));
+  return res.json(await userService.findById(req));
 };
 
-// route /api/v1/users
+// route /api/v1/users && /api/v1/register
 // create a new user
-exports.create = async (req, res, next) => {
-  const user = await userService.create(req.body);
-  return res.status(201).json(user);
+exports.create = async (req, res, next) => {  
+  return res.status(201).json(await userService.create(req));
 };
 
 // route /api/v1/users/:id
 // update a user using id
 exports.update = async (req, res, next) => {
-  const id = req.params.id;
-  const user = await userService.update(id, req.body);
-  return res.status(200).json(user);
+  return res.status(200).json(await userService.update(req));
 };
 
 // route /api/v1/users/:id
 // delete a user using id
 exports.remove = async (req, res, next) => {
-  const id = req.params.id;
-  const user = await userService.remove(id);
-  return res.status(200).json(user);
+  return res.status(200).json(await userService.remove(req));
+};
+
+// route /api/v1/login
+// login a user
+exports.login = async (req, res, next) => {
+  return res.json(await userService.login(req));
 };
