26e14586f71e19f6f1278d6b6da854b104964af1
Ahmed Abuwarda <zzz.sleep.com@gmail.com>
2022-10-06 19:17:51 +0300

    retype all the routes

--- routes/v1/userRouter.js
+++ routes/v1/userRouter.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const isAuth = require('../../middlewares/v1/isAuth.guard.js');
+const is = require('../../middlewares/v1/is.guard.js');
 
 const userController = require('../../controllers/v1/userController.js');
 const errorMiddleware = require('../../middlewares/v1/errorMiddleware.js');
@@ -7,14 +9,29 @@ const userRouter = express.Router();
 
 userRouter
   .route('/')
-  .get(errorMiddleware(userController.index))
-  .post(errorMiddleware(userController.create));
+  .get(
+    [isAuth, is.admin],
+    errorMiddleware(userController.index)
+  ) // /api/v1/users
+  .post(
+    [is.guest],
+    errorMiddleware(userController.create)
+  ); // /api/v1/users
 
 userRouter
   .route('/:id')
-  .get(errorMiddleware(userController.show))
-  .put(errorMiddleware(userController.update))
-  .delete(errorMiddleware(userController.remove));
+  .get(
+    [isAuth, is.admin_or_user],
+    errorMiddleware(userController.show)
+  )
+  .put(
+    [isAuth, is.admin_or_user],
+    errorMiddleware(userController.update)
+  )
+  .delete(
+    [isAuth, is.admin_or_user],
+    errorMiddleware(userController.remove)
+  );
 
 // export user router
 module.exports = userRouter;
